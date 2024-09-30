28c9aaa9838e74dac8cbac1ed16932ab23baf02f
Gustavo <oakhenry2@gmail.com>
2024-09-30 15:46:36 -0300

    Modificações novas

--- src/Main.java
+++ src/Main.java
@@ -3,86 +3,53 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Declaração e instanciação dos objetos
-        Paciente paciente1 = new Paciente("Maria Souza",  "10/10/1990", "999999999", "Ansiedade");
+        // Criando Pacientes
+        Paciente paciente1 = new Paciente("Maria Souza", "10/10/1990", "999999999", "Ansiedade");
         Paciente paciente2 = new Paciente("Carlos Pereira", "20/05/1985", "888888888", "Depressão");
 
-        Psicologo psicologo1 = new Psicologo("Ana Silva", "01/01/1980",  "777777777", "CRP-12345", "Terapia Cognitivo-Comportamental");
+        // Criando Psicólogos
+        Psicologo psicologo1 = new Psicologo("Ana Silva", "01/01/1980", "777777777", "CRP-12345", "Terapia Cognitivo-Comportamental");
         Psicologo psicologo2 = new Psicologo("João Oliveira", "15/08/1978", "666666666", "CRP-67890", "Psicanálise");
 
-        ClinicaPsicologica clinica1 = new ClinicaPsicologica("Clínica Bem-Estar");
+        // Agendando Consultas
+        psicologo1.agendarConsulta(paciente1, "10/10/2024", "15:00", "Agendada", "Online");
+        psicologo2.agendarConsulta(paciente2, "15/10/2024", "10:00", "Agendada", "Presencial");
 
-        // Adicionando Psicólogos à Clínica
-        clinica1.adicionarPsicologo(psicologo1);
-        clinica1.adicionarPsicologo(psicologo2);
-
-        // Exibindo informações dos pacientes
-        System.out.println("---- Dados do paciente 1 ----");
-        System.out.println("Nome: " + paciente1.getNome());
-        System.out.println("Data de Nascimento: " + paciente1.getDataNascimento());
-        System.out.println("Número: " + paciente1.getNumero());
-        System.out.println("Motivo da Consulta: " + paciente1.getMotivoConsulta());
+        // Exibindo informações dos pacientes e consultas
+        System.out.println("\n---- Dados do paciente 1 ----");
+        System.out.println(paciente1);
+        for (Consulta consulta : psicologo1.getConsultas()) {
+            if (consulta.getPaciente() == paciente1) {
+                consulta.exibirInformacoes();
+            }
+        }
 
         System.out.println("\n---- Dados do paciente 2 ----");
-        System.out.println("Nome: " + paciente2.getNome());
-        System.out.println("Data de Nascimento: " + paciente2.getDataNascimento());
-        System.out.println("Número: " + paciente2.getNumero());
-        System.out.println("Motivo da Consulta: " + paciente2.getMotivoConsulta());
+        System.out.println(paciente2);
+        for (Consulta consulta : psicologo2.getConsultas()) {
+            if (consulta.getPaciente() == paciente2) {
+                consulta.exibirInformacoes();
+            }
+        }
 
         // Modificando dados do paciente 1
         paciente1.setNome("Maria Silva Souza");
-        paciente1.setDataNascimento("11/11/1991");
-        paciente1.setNumero("987654321");
-        paciente1.setMotivoConsulta("Ansiedade Generalizada");
 
         System.out.println("\n---- Dados do paciente 1 (após modificação) ----");
-        System.out.println("Nome: " + paciente1.getNome());
-        System.out.println("Data de Nascimento: " + paciente1.getDataNascimento());
-        System.out.println("Número: " + paciente1.getNumero());
-        System.out.println("Motivo da Consulta: " + paciente1.getMotivoConsulta());
+        System.out.println(paciente1);
 
         // Exibindo informações dos psicólogos
         System.out.println("\n---- Dados do psicólogo 1 ----");
-        System.out.println("Nome: " + psicologo1.getNome());
-        System.out.println("Data de Nascimento: " + psicologo1.getDataNascimento());
-        System.out.println("Número: " + psicologo1.getNumero());
-        System.out.println("CRP: " + psicologo1.getCrp());
-        System.out.println("Especialidade: " + psicologo1.getEspecialidade());
+        System.out.println(psicologo1);
 
         System.out.println("\n---- Dados do psicólogo 2 ----");
-        System.out.println("Nome: " + psicologo2.getNome());
-        System.out.println("Data de Nascimento: " + psicologo2.getDataNascimento());
-        System.out.println("Número: " + psicologo2.getNumero());
-        System.out.println("CRP: " + psicologo2.getCrp());
-        System.out.println("Especialidade: " + psicologo2.getEspecialidade());
+        System.out.println(psicologo2);
 
         // Modificando dados do psicólogo 2
         psicologo2.setNome("João dos Santos Oliveira");
-        psicologo2.setDataNascimento("16/09/1979");
-        psicologo2.setNumero("654321987");
 
         System.out.println("\n---- Dados do psicólogo 2 (após modificação) ----");
-        System.out.println("Nome: " + psicologo2.getNome());
-        System.out.println("Data de Nascimento: " + psicologo2.getDataNascimento());
-        System.out.println("Número: " + psicologo2.getNumero());
-        System.out.println("CRP: " + psicologo2.getCrp());
-        System.out.println("Especialidade: " + psicologo2.getEspecialidade());
-
-        // Exibindo informações da clínica
-        System.out.println("\n---- Informações da clínica 1 ----");
-        System.out.println("Nome: " + clinica1.getNome());
-
-
-        // Modificando nome da clínica
-        clinica1.setNome("Clínica Bem-Estar Total");
-
-        System.out.println("\n---- Informações da clínica 1 (após modificação) ----");
-        System.out.println("Nome: " + clinica1.getNome());
-
-
-        // Agendando Consultas (demonstração)
-        clinica1.agendarConsulta(psicologo1, paciente1, "10/10/2024", "15:00", "Agendada", "Online");
-        clinica1.agendarConsulta(psicologo2, paciente2, "15/10/2024", "10:00", "Agendada", "Presencial");
+        System.out.println(psicologo2);
     }
 }
 
@@ -91,7 +58,7 @@ class Pessoa {
     protected String dataNascimento;
     protected String numero;
 
-    public Pessoa(){
+    public Pessoa() {
         nome = "Jorge";
         dataNascimento = "01/01/1999";
         numero = "12345";
@@ -156,8 +123,27 @@ class Psicologo extends Pessoa {
         this.especialidade = especialidade;
     }
 
-    public void adicionarConsulta(Consulta consulta) {
-        consultas.add(consulta);
+    public List<Consulta> getConsultas(){
+        return consultas;
+    }
+
+    public void setConsultas(List<Consulta> consultas){
+        this.consultas = consultas;
+    }
+
+    public void agendarConsulta(Paciente paciente, String data, String hora, String status, String tipo) {
+        Consulta consulta = new Consulta(this, paciente, data, hora, status, tipo);
+        this.consultas.add(consulta);
+    }
+
+    @Override
+    public String toString() {
+        return "Psicólogo: " + nome +
+                "\nData de Nascimento: " + dataNascimento +
+                "\nNúmero: " + numero +
+                "\nCRP: " + crp +
+                "\nEspecialidade: " + especialidade +
+                "\nConsultas agendadas: " + consultas.size();
     }
 }
 
@@ -175,6 +161,14 @@ class Paciente extends Pessoa {
 
     public void setMotivoConsulta(String motivoConsulta) {
         this.motivoConsulta = motivoConsulta;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente: " + nome +
+                "\nData de Nascimento: " + dataNascimento +
+                "\nNúmero: " + numero +
+                "\nMotivo da Consulta: " + motivoConsulta;
     }
 }
 
@@ -197,64 +191,23 @@ class Consulta {
                 " para " + paciente.getNome() + " em " + data + " às " + hora + ".");
     }
 
-    // Getters para os atributos
-    public String getData() {
-        return data;
+    public Paciente getPaciente(){
+        return paciente;
     }
 
-    public String getHora() {
-        return hora;
+    public void setPaciente(Paciente paciente){
+        this.paciente = paciente;
     }
 
-    public String getStatus() {
-        return status;
-    }
 
-    public String getTipo() {
-        return tipo;
-    }
-
-    // Setters para os atributos (se necessário)
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public void setHora(String hora) {
-        this.hora = hora;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-}
-
-class ClinicaPsicologica {
-    private String nome;
-    private List<Psicologo> psicologos;
-
-    public ClinicaPsicologica(String nome) {
-        this.nome = nome;
-        this.psicologos = new ArrayList<>();
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void adicionarPsicologo(Psicologo psicologo) {
-        psicologos.add(psicologo);
-    }
-
-    public void agendarConsulta(Psicologo psicologo, Paciente paciente, String data, String hora, String status, String tipo) {
-        Consulta consulta = new Consulta(psicologo, paciente, data, hora, status, tipo);
-        psicologo.adicionarConsulta(consulta);
+    // Método para exibir informações da consulta
+    public void exibirInformacoes() {
+        System.out.println("  Dados da Consulta:");
+        System.out.println("    Psicólogo: " + psicologo.getNome());
+        System.out.println("    Paciente: " + paciente.getNome());
+        System.out.println("    Data: " + data);
+        System.out.println("    Hora: " + hora);
+        System.out.println("    Status: " + status);
+        System.out.println("    Tipo: " + tipo);
     }
 }
